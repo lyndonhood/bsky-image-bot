438637b8e533c1a1956d80376194382275b4ab9e
lyndonhood <lyndon.hood@gmail.com>
2023-11-19 16:54:55 +1300

    Update index.ts
    back to orginal, see if that helps

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,24 @@ dotenv.config();
 
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
+  // Remove the file extension and parse the date
+  const dateParts = imageName.replace('.jpg', '').split('-');
+  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
 
-  // Just skip all that and return chosen text
-  return 'post text';
+  // Create a formatter
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  // Format the date
+  return formatter.format(date);
 }
 
 // EDIT THIS!
 function altTextFromImageName(imageName: string): string {
-  // return 'Image from ' + postTextFromImageName(imageName);
-  return 'Daniel Craig introducing The Weekend on SNL, with the text: E rangatira mā... Te mutunga wiki';
+  return 'Image from ' + postTextFromImageName(imageName);
 }
 
 // Shouldn't have to edit this.
