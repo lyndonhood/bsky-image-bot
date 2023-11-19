9bf5f043a9282ed331a6dfa015543aa3a1312774
lyndonhood <lyndon.hood@gmail.com>
2023-11-19 17:12:48 +1300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,7 @@ dotenv.config();
 
 // EDIT THIS!
 function postTextFromImageName(imageName) {
-  // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
-
-  // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-
-  // Format the date
-  return formatter.format(date);
+  return 'kia ora world';
 }
 
 // EDIT THIS!
