a2a2c4db598e13f3da8dffcf31d60108e6a28cfe
lyndonhood <lyndon.hood@gmail.com>
2023-11-19 14:22:27 +1300

    Update index.ts
    Remove post text and use hard coded alt text

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,27 @@ dotenv.config();
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
   // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
+  // const dateParts = imageName.replace('.jpg', '').split('-');
+  // const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
 
   // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
+  // const formatter = new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
 
   // Format the date
-  return formatter.format(date);
+  // return formatter.format(date);
+
+  // Just skip all that and return chosen text
+  return '';
 }
 
 // EDIT THIS!
 function altTextFromImageName(imageName: string): string {
-  return 'Image from ' + postTextFromImageName(imageName);
+  // return 'Image from ' + postTextFromImageName(imageName);
+  return 'Daniel Craig introducting The Weekend on SNL, with the text: E rangatira mā - Te mutunga wiki';
 }
 
 // Shouldn't have to edit this.
